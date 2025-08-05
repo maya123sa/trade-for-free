5655111df2dfad759119e296cf4700fc80b31921
sanketmarghade <73730404+sanketmarghade@users.noreply.github.com>
2025-08-05 22:06:04 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Install TA-Lib dependencies
+wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
+tar -xvzf ta-lib-0.4.0-src.tar.gz
+cd ta-lib/
+./configure --prefix=/usr
+make
+sudo make install
